8e8d7dc1bc49f64e2af6edcdfe51cb0e2d8e9e9b
SanyogDeshmukh <72301054+SanyogDeshmukh@users.noreply.github.com>
2022-02-02 13:18:57 +0530

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return
+def substract(x,y):
+    return x-y #on master
+def multiply(x,y):
+    return  x*y #on bug456
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    else:                  
+        return x/y    
+#Square  implementation
+def square(x):
+    pass
+
